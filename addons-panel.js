5b8a43ace7b476f44ca7d53072470f9fa4967e85
Konrad <k.konrad9902@gmail.com>
2023-09-11 14:10:37 +0200

    test await

--- addons-panel.js
+++ addons-panel.js
@@ -1,13 +1,12 @@
-(function () {
-    // fetch('https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js')
-    //     .then(res => res.blob())
-    //     .then(blob => {
-    //         var objectURL = URL.createObjectURL(blob)
-    //         var sc = document.createElement("script")
-    //         sc.setAttribute("src", objectURL)
-    //         sc.setAttribute("type", "text/javascript")
-    //         document.head.appendChild(sc)
-    //     })
-    $.getScript('https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js')
-    console.log('addons-panel.js')
+(async function () {
+    await fetch('https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js')
+        .then(res => res.blob())
+        .then(blob => {
+            var objectURL = URL.createObjectURL(blob)
+            var sc = document.createElement("script")
+            sc.setAttribute("src", objectURL)
+            sc.setAttribute("type", "text/javascript")
+            document.head.appendChild(sc)
+        })
+    console.log(Constants.addons)
 })()
