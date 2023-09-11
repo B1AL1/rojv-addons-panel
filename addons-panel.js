2f14e2af55c453142361e29fa67fbf67e738909d
Konrad <k.konrad9902@gmail.com>
2023-09-11 14:09:28 +0200

    jq test

--- addons-panel.js
+++ addons-panel.js
@@ -1,12 +1,13 @@
 (function () {
-    fetch('https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js')
-        .then(res => res.blob())
-        .then(blob => {
-            var objectURL = URL.createObjectURL(blob)
-            var sc = document.createElement("script")
-            sc.setAttribute("src", objectURL)
-            sc.setAttribute("type", "text/javascript")
-            document.head.appendChild(sc)
-        })
+    // fetch('https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js')
+    //     .then(res => res.blob())
+    //     .then(blob => {
+    //         var objectURL = URL.createObjectURL(blob)
+    //         var sc = document.createElement("script")
+    //         sc.setAttribute("src", objectURL)
+    //         sc.setAttribute("type", "text/javascript")
+    //         document.head.appendChild(sc)
+    //     })
+    $.getScript('https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js')
     console.log('addons-panel.js')
 })()
