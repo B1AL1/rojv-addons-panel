05cc01626e4f565e25851eabcbc13fccf8f8e1fd
Konrad <k.konrad9902@gmail.com>
2023-09-11 14:00:33 +0200

    test fetch

--- addons-panel.js
+++ addons-panel.js
@@ -1,5 +1,12 @@
-import Constants from 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js'
-
 (function () {
-    console.log(Constants.addons)
+    fetch('https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js')
+        .then(res => res.blob())
+        .then(blob => {
+            var objectURL = URL.createObjectURL(blob)
+            var sc = document.createElement("script")
+            sc.setAttribute("src", objectURL)
+            sc.setAttribute("type", "text/javascript")
+            document.head.appendChild(sc)
+        })
+    console.log('addons-panel.js')
 })()
