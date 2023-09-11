28a98702612da776d64323fcb979db2c036938eb
Konrad <k.konrad9902@gmail.com>
2023-09-11 14:14:15 +0200

    removed constants

--- addons-panel.js
+++ addons-panel.js
@@ -1,12 +1,51 @@
-(async function () {
-    await fetch('https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/utils/constants.js')
-        .then(res => res.blob())
-        .then(blob => {
-            var objectURL = URL.createObjectURL(blob)
-            var sc = document.createElement("script")
-            sc.setAttribute("src", objectURL)
-            sc.setAttribute("type", "text/javascript")
-            document.head.appendChild(sc)
-        })
-    console.log(Constants.addons)
-})()
+const addons = {
+    'autoheal-fixed': {
+        name: 'Autoheal Fixed',
+        description: 'Autoheal Fixed',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/autoheal-fixed.js'
+    },
+    'clan-members-online-fixed': {
+        name: 'Clan Members Online Fixed',
+        description: 'Clan Members Online Fixed',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/clan-members-online-fixed.js'
+    },
+    'colors-of-linked-items': {
+        name: 'Colors of Linked Items',
+        description: 'Colors of Linked Items',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/colors-of-linked-items.js'
+    },
+    'enhancement-upgrade-lvl': {
+        name: 'Enhancement Upgrade Lvl',
+        description: 'Enhancement Upgrade Lvl',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/enhancement-upgrade-lvl.js'
+    },
+    'inform-chat': {
+        name: 'Inform Chat',
+        description: 'Inform Chat',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/inform-chat.js'
+    },
+    'local-chat-by-default': {
+        name: 'Local Chat by Default',
+        description: 'Local Chat by Default',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/local-chat-by-default.js'
+    },
+    'relog-enhancer': {
+        name: 'Relog Enhancer',
+        description: 'Relog Enhancer',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/relog-enhancer.js'
+    },
+    'rewards-calendar-reminder': {
+        name: 'Rewards Calendar Reminder',
+        description: 'Rewards Calendar Reminder',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/rewards-calendar-reminder.js'
+    },
+    'unlag-invite': {
+        name: 'Unlag Invite',
+        description: 'Unlag Invite',
+        url: 'https://raw.githubusercontent.com/B1AL1/async-addons-panel/main/addons/unlag-invite.js'
+    }
+}
+
+    (function () {
+        console.log(addons)
+    })()
