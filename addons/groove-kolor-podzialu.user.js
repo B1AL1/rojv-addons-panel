b6595dba90da25f00a8b0f7b1344542a6384dd68
Konrad <k.konrad9902@gmail.com>
2024-03-23 12:50:04 +0100

    fix: typo

--- addons/groove-kolor-podzialu.user.js
+++ addons/groove-kolor-podzialu.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Groove Kolor podziału
+// @name         Groove Kolor Podziału
 // @version      1.0
 // @description  Zmienia kolor podziału łupu, w zależności od rangi przedmiotu
 // @author       Rojvu
